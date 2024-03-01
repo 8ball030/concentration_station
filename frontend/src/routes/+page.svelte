03ae5fb48ccf8b5c65640447bf4936c0445c0fce
asyaasha7 <asya.tuli@gmail.com>
2024-02-29 19:21:06 -0700

    [fix] remove coin promise

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -4,14 +4,12 @@
 	import { fly } from 'svelte/transition'
 	import { quintOut } from 'svelte/easing';
 	import { ProgressBar } from '@skeletonlabs/skeleton';
-    import { getModalStore } from '@skeletonlabs/skeleton';
+	import { getModalStore } from '@skeletonlabs/skeleton';
 	import io from 'socket.io-client';
 
 	import TransactionFrame from '$lib/components/TransactionFrame.svelte';
 	import Card from '$lib/components/Card.svelte';
 	import {state} from '$lib/stores'
-	import submitBuy from '$lib/actions/submitBuy';
-	import loadCoins from '$lib/actions/getCoinsList';
 	import {getCurrentCoin, getLedgers, postSwap} from '$lib/actions';
 	import {SOCKET_URL, DEFAULT_CHAIN, INTENTION_DIRECTIONS} from '$lib/consts'
 	import {mockCoin} from '$lib/mock'
@@ -31,49 +29,42 @@
 
 	let cardDummyList = [0]
 	let index = 0
-	let currentCoinPromise = Promise.resolve({})
 	let coin = {}
   	let animate = false
 	let loading = false
 
-	// we want to call connect on the socket
-	socket.connect();
+	onMount(() =>{
+		// we want to call connect on the socket
+		socket.connect();
 
-	socket.on('connect', () => {
-		console.log('Successfully connected to the agent!')
-		const setting = {
-			message: 'Successfully connected to the agent!',
-			timeout: 3000,
-			background: 'variant-ghost-success'
-		};
+		socket.on('connect', () => {
+			console.log('Successfully connected to the agent!')
+			const setting = {
+				message: 'Successfully connected to the agent!',
+				timeout: 3000,
+				background: 'variant-ghost-success'
+			};
 
-		connection_status = true;
-		socket.emit('agent', 'agent');
-	});
+			connection_status = true;
+			socket.emit('agent', 'agent');
+		});
 
-	socket.on('disconnect', () => {
-		connection_status = false;
-		const setting = {
-			message: 'Disconnected from the agent!',
-			timeout: 3000,
-			background: 'variant-ghost-error'
-		};
-	});
+		socket.on('disconnect', () => {
+			connection_status = false;
+			const setting = {
+				message: 'Disconnected from the agent!',
+				timeout: 3000,
+				background: 'variant-ghost-error'
+			};
+		});
 
-	socket.on('packet', (data, type) => {
-		console.log('packet', data)
-	});
-	
+		socket.on('data', (data) => {
+			console.log('intention', data)
+			
+			//handleIntention(data?.intention);
+		});
+	})
 
-	socket.on('data', (data) => {
-		console.log('intention', data)
-		if (!streamLogsChecked) {
-			return;
-		}
-		
-		handleIntention(data?.intention);
-	});
-	
 	state.subscribe((value) => {
 		cardDummyList = value;
 	});
@@ -92,7 +83,7 @@
 
 			const res = await postSwap(coin.id, DEFAULT_CHAIN)
 			if (res) {
-				currentCoinPromise = getCurrentCoin()
+				getCoin()
 				handleSwapPosted()
 			}
 			loading = false
@@ -116,43 +107,44 @@
 		func()
 	}
 
+	async function getCoin(){
+		coin = await getCurrentCoin()
+	}
+
 	onMount(() => {
-		getLedgers()
-		currentCoinPromise = getCurrentCoin()
+		// getLedgers()
+		getCoin()
 	});
 </script>
 
-{#await currentCoinPromise then currentCoinPromise}
-	{setCoin(() => { coin = currentCoinPromise; })}
-	<div class="success">
-		{#if animate}
-			<div
-				transition:fly={
-					{ 
-						delay: 0,
-						duration: 100,
-						x: -50,
-						y: 200,
-						opacity: 0.5,
-						easing: quintOut 
-					}
+<div class="success">
+	{#if animate}
+		<div
+			transition:fly={
+				{ 
+					delay: 0,
+					duration: 100,
+					x: -50,
+					y: 200,
+					opacity: 0.5,
+					easing: quintOut 
 				}
-				>
-				👍🏼 
-			</div>
-		{/if}
-	</div>
-	<div class="stack grid place-items-center mt-40">
-		{#each cardDummyList as dummy (dummy)}
-			<Card coin={currentCoinPromise} onbuttonTapped={handleIntention} />
-		{/each}
-		{#if loading}
-			<div class="w-96">
-				<ProgressBar value={undefined} />
-			</div>
-		{/if}
-	</div>
-{/await}
+			}
+			>
+			👍🏼 
+		</div>
+	{/if}
+</div>
+<div class="stack grid place-items-center mt-40">
+	{#each cardDummyList as dummy (dummy)}
+		<Card coin={coin} onbuttonTapped={handleIntention} />
+	{/each}
+	{#if loading}
+		<div class="w-96">
+			<ProgressBar value={undefined} />
+		</div>
+	{/if}
+</div>
 
 <style>
 	.success {
